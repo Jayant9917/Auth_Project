d3b6d3c524e4a86b8dbb662ddf5b295bf64d0907
Jayant9917 <ranajayant527@gmail.com>
2025-07-25 01:18:45 +0530

    home page Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import EmailVerify from './pages/EmailVerify.jsx'
+import ResetPassword from './pages/ResetPassword.jsx'
+
 
 const App = () => {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/email-verify' element={<EmailVerify/>}/>
+        <Route path='/reset-password' element={<ResetPassword/>}/>
+      </Routes>
     </div>
   )
 }
