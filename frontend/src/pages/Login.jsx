419f01a29cac7128bcfcd69ede5f50a3234daeee
Jayant9917 <ranajayant527@gmail.com>
2025-07-26 18:02:07 +0530

    Login Page Updation

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,7 +12,9 @@ const Login = () => {
         className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
       />
       <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm">
-        <h2 className="text-3xl font-semibold text-white text-center mb-3">{state === "Sign Up" ? "Create Account" : "Login"}</h2>
+        <h2 className="text-3xl font-semibold text-white text-center mb-3">
+          {state === "Sign Up" ? "Create Account" : "Login"}
+        </h2>
         <p className="text-center text-sm mb-6">
           {state === "Sign Up"
             ? "Create your account"
@@ -48,7 +50,12 @@ const Login = () => {
             />
           </div>
 
-          <p className="mb-4 text-indigo-500 cursor-pointer">Forget Password?</p>
+          <p className="mb-4 text-indigo-500 cursor-pointer">
+            Forget Password?
+          </p>
+          <button>
+            {state}
+          </button>
         </form>
       </div>
     </div>
